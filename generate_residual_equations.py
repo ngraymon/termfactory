30e7b401b94bfc457a5630fa5bfe03853b9b92c0
Neil <ngraymon@uwaterloo.ca>
2021-05-20 13:50:01 -0400

    Move these away

--- generate_residual_equations.py
+++ generate_residual_equations.py
@@ -16,6 +16,27 @@ import reference_latex_headers as headers
 tab_length = 4
 tab = " "*tab_length
 
+# use these if we define \newcommand to map textbf to \bt and so forth
+if True:
+    bold_t_latex = "\\bt"
+    bold_h_latex = "\\bh"
+    bold_w_latex = "\\bw"
+    bold_c_latex = "\\bc"
+    bold_d_latex = "\\bd"
+    bold_z_latex = "\\bz"
+    bold_s_latex = "\\bs"
+    bold_G_latex = "\\bG"
+else:
+    bold_t_latex = "\\textbf{t}"
+    bold_h_latex = "\\textbf{h}"
+    bold_w_latex = "\\textbf{w}"
+    bold_c_latex = "\\textbf{c}"
+    bold_d_latex = "\\textbf{d}"
+    bold_z_latex = "\\textbf{z}"
+    bold_s_latex = "\\textbf{s}"
+    bold_G_latex = "\\textbf{G}"
+
+
 # our Hamiltonian is
 # H = (h_0 + omega + h^1 + h_1) + h^1_1 + h^2 + h_2
 # but we can ignore the omega when calculating residuals as we add it back in at a later point
@@ -445,28 +466,6 @@ def generate_residual_data(H, max_order):
 # ----------------------------------------------------------------------------------------------- #
 # -------------------------------  GENERATING RESIDUAL LATEX  ----------------------------------- #
 # ----------------------------------------------------------------------------------------------- #
-
-
-# use these if we define \newcommand to map textbf to \bt and so forth
-if True:
-    bold_t_latex = "\\bt"
-    bold_h_latex = "\\bh"
-    bold_w_latex = "\\bw"
-    bold_c_latex = "\\bc"
-    bold_d_latex = "\\bd"
-    bold_z_latex = "\\bz"
-    bold_s_latex = "\\bs"
-    bold_G_latex = "\\bG"
-else:
-    bold_t_latex = "\\textbf{t}"
-    bold_h_latex = "\\textbf{h}"
-    bold_w_latex = "\\textbf{w}"
-    bold_c_latex = "\\textbf{c}"
-    bold_d_latex = "\\textbf{d}"
-    bold_z_latex = "\\textbf{z}"
-    bold_s_latex = "\\textbf{s}"
-    bold_G_latex = "\\textbf{G}"
-
 
 # ----------------- generating VECI residual latex -------------------- #
 def _generate_veci_explicit_latex_term(term_list, h_list, w_string, order):
@@ -4319,7 +4318,7 @@ def _contracted_expressions(partition_list, order):
         exp_list.append([max(partition), ] + temp_list)
 
     return exp_list
-    
+
 
 def _write_optimized_vemx_paths_function(max_order):
     """Return strings to write all the `oe.contract_expression` calls.
