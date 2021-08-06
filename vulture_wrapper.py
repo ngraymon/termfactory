176fca3bf881a597b8fb7765bf9125bc6540b95d
Neil <ngraymon@uwaterloo.ca>
2021-08-06 14:02:10 -0400

    trying to capture vulture output

--- vulture_wrapper.py
+++ vulture_wrapper.py
@@ -4,6 +4,7 @@ import os
 if (__name__ == '__main__'):
 
     sys.stdout = open('vulture_output.txt', 'w')
+    sys.stderr = sys.stdout
 
     os.system(
         "py -m vulture --min-confidence 100 generate_residual_equations.py"
