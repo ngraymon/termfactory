de194d887ba640b5c9e822cea3b77505f4355b4e
Neil <ngraymon@uwaterloo.ca>
2021-08-06 14:16:43 -0400

    trying subprocess

--- vulture_wrapper.py
+++ vulture_wrapper.py
@@ -1,14 +1,20 @@
+import subprocess
 import sys
-import os
 
 if (__name__ == '__main__'):
 
-    sys.stdout = open('vulture_output.txt', 'w')
-    sys.stderr = sys.stdout
+    # sys.stdout = open('vulture_output.txt', 'w')
+    # sys.stderr = sys.stdout
 
-    print("testing stdout")
+    # print("testing stdout")
 
-    os.system(
-        "py -m vulture --min-confidence 100 generate_residual_equations.py"
+    ret = subprocess.run(
+        ['py', '-m', 'vulture', '--min-confidence 100', 'generate_residual_equations.py'],
+        capture_output=True,
     )
-    sys.stdout.close()
+
+    with open('vulture_output.txt', 'w') as fp:
+        fp.write(ret.stdout)
+        fp.write(ret.stderr)
+
+    # sys.stdout.close()
