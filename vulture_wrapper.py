92bb8e51d72e8e11adc8894669d9a8e49880b966
Neil <ngraymon@uwaterloo.ca>
2021-08-06 13:56:03 -0400

    Wrapping vulture to capture output?

---
+++ vulture_wrapper.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+if (__name__ == '__main__'):
+
+    sys.stdout = open('vulture_output.txt', 'w')
+
+    os.system(
+        "vulture --min-confidence 100 generate_residual_equations.py"
+    )
+    sys.stdout.close()
