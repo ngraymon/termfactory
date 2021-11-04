8b8936ac347f1546834d1498df6089057ded2c6a
Neil <ngraymon@uwaterloo.ca>
2021-11-04 10:58:36 -0400

    Adding T^0 operator and changing prefactor generator

--- driver.py
+++ driver.py
@@ -125,7 +125,7 @@ if (__name__ == '__main__'):
     truncations = maximum_h_rank, maximum_cc_rank, s_taylor_max_order, omega_max_order
 
     # for the 'eT_z_t ansatz' only
-    maximum_h_rank = 2
+    maximum_h_rank = 3
     maximum_cc_rank = 2
     maximum_T_rank = 2
     eT_taylor_max_order = 2
@@ -134,7 +134,7 @@ if (__name__ == '__main__'):
     # need to have truncation of e^T
     eT_z_t_truncations = maximum_h_rank, maximum_cc_rank, maximum_T_rank, eT_taylor_max_order, omega_max_order
 
-    if False:
+    if True:
         generate_latex_files(
             eT_z_t_truncations,
             only_ground_state=True,
