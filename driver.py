f7bae149678635a9b731c20f8e4929bd68d298ec
Neil <ngraymon@uwaterloo.ca>
2021-12-16 17:32:29 -0500

    Attempted to add argparse
    of course this won't actually work because the logger setup is wrong
    see lines 23-25 in `latex_eT_zhz.py`
    
    log = log_conf.get_filebased_logger('output.txt')
    header_log = log_conf.HeaderAdapter(log, {})
    subheader_log = log_conf.SubHeaderAdapter(log, {})
    
    where just by importing this module we've permanetly forced log to 
    be tied to `output.txt`
    
    need to consult the documentation more
    https://docs.python.org/3/howto/logging-cookbook.html#formatting-styles

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 # system imports
 import os
 import sys
+import argparse
 
 # third party imports
 
@@ -100,15 +101,31 @@ def dump_all_stdout_to_devnull():
     sys.stdout = open(os.devnull, 'w')
 
 
+def prepare_parsed_arguments():
+    """ Wrapper for argparser setup """
+
+    # formatclass = argparse.RawDescriptionHelpFormatter
+    # formatclass = argparse.RawTextHelpFormatter
+    formatclass = argparse.ArgumentDefaultsHelpFormatter  # I liked this the best
+    # formatclass = argparse.MetavarTypeHelpFormatter
+
+    # parse the arguments
+    parser = argparse.ArgumentParser(description="Code/Latex Generator", formatter_class=formatclass)
+    parser.add_argument('-l', '--log_path', type=str, metavar='/path/filename.txt', help='path to log file')
+
+    return parser.parse_args()
+
+
 if (__name__ == '__main__'):
     import log_conf
 
-    # for now make a second argument the filepath for logging output
-    if len(sys.argv) > 1:
-        logging_output_filename = str(sys.argv[1])
-        log = log_conf.get_filebased_logger(logging_output_filename)
-    else:
+    pargs = prepare_parsed_arguments()
+
+    if pargs.log_path is None:
         log = log_conf.get_stdout_logger()
+    else:
+        logging_output_filename = str(pargs.log_path)
+        log = log_conf.get_filebased_logger(logging_output_filename)
 
     header_log = log_conf.HeaderAdapter(log, {})
     subheader_log = log_conf.SubHeaderAdapter(log, {})
