99810011c749a75fd13d58e348c450b80cc27443
Neil <ngraymon@uwaterloo.ca>
2022-03-10 15:48:28 -0500

    Layout scheme for latex test folder

---
+++ tests/theory_check/context.py
@@ -0,0 +1,7 @@
+from os import pardir
+from os.path import abspath, join, dirname
+import sys
+
+# import the path to the pibronic package
+my_directory = dirname(__file__)
+sys.path.insert(0, abspath(join(my_directory, pardir, pardir)))
