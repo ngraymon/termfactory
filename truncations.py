2c5b057757c0857addb847bf0b334ee8058c25da
lucas <lhprice@uwaterloo.ca>
2022-03-31 02:54:47 -0400

    Issue 15 - treating truncations as either named tuple or dict -essentially functional implementation of treating truncations as either a dict or named tuple

---
+++ truncations.py
@@ -0,0 +1,103 @@
+# system imports
+import os
+import itertools as it
+from collections import namedtuple
+
+# third party imports
+
+# local imports
+
+truncation_maximums = {
+    'maximum_hamiltonian_rank': 6,  # maximum_h_rank
+    'maximum_coupled_cluster_rank': 6,  # maximum_cc_rank
+    's_taylor_max_order': 6,
+    'omega_max_order': 6,
+    'max_residual_order': 8,
+    'max_w_order': 8,
+    'dt_order': 8,
+}
+
+################################################################################
+"""
+defines namedtuple's for the different truncation types
+"""
+################################################################################
+"""
+full cc type:
+
+maximum_h_rank, maximum_cc_rank, s_taylor_max_order, omega_max_order = truncations
+
+eT_z_t type:
+
+maximum_h_rank, maximum_cc_rank, maximum_T_rank, eT_taylor_max_order, omega_max_order = truncations
+
+"""
+
+fcc_truncations_namedtuple = namedtuple(
+    'fcc_truncations',
+    [
+        'maximum_hamiltonian_rank',
+        'maximum_coupled_cluster_rank',
+        's_taylor_max_order',
+        'omega_max_order'
+    ]
+)
+eT_z_t_truncations_namedtuple = namedtuple(
+    'eT_z_t_truncations',
+    [
+        'maximum_hamiltonian_rank',
+        'maximum_coupled_cluster_rank',
+        'maximum_T_rank',
+        'eT_taylor_max_order',
+        'omega_max_order'
+    ]
+)
+###################################################################
+"""
+Verify concept
+"""
+# below assumes truncations is type==dictionary
+
+def _verify_fcc_truncations(truncations):
+    for key in ["maximum_hamiltonian_rank", "maximum_coupled_cluster_rank", "s_taylor_max_order", "omega_max_order"]:
+        assert key in truncations, f"Missing key, {key = :s} not in provided dictionary {truncations=:}"
+        assert truncations[key] >= 1, "Truncations need to be positive integers"
+        assert truncations[key] <= truncation_maximums[key], f"Key {key} is over the maximum of {truncation_maximums[key]}"
+
+    # add unpack function? like _unpack_fcc_truncations
+    # unpack functions would call its respective _verify_x function
+    maximum_hamiltonian_rank = truncations["maximum_hamiltonian_rank"]
+    maximum_coupled_cluster_rank = truncations["maximum_coupled_cluster_rank"]
+    s_taylor_max_order = truncations["s_taylor_max_order"]
+    omega_max_order = truncations["omega_max_order"]
+
+
+def _verify_eT_z_t_truncations(truncations):
+    for key in ["maximum_hamiltonian_rank", "maximum_coupled_cluster_rank", "maximum_T_rank", "eT_taylor_max_order", "omega_max_order"]:
+        assert key in truncations, f"Missing key, {key = :s} not in provided dictionary {truncations=:}"
+        assert truncations[key] >= 1, "Truncations need to be positive integers"
+        assert truncations[key] <= truncation_maximums[key], f"Key {key} is over the maximum of {truncation_maximums[key]}"
+
+    # add unpack function? like _unpack_eT_z_t_truncations
+    maximum_hamiltonian_rank = truncations["maximum_hamiltonian_rank"]
+    maximum_coupled_cluster_rank = truncations["maximum_coupled_cluster_rank"]
+    maximum_T_rank = truncations["maximum_T_rank"]
+    eT_taylor_max_order = truncations["eT_taylor_max_order"]
+    omega_max_order = truncations["omega_max_order "]
+
+
+# test={
+#     'maximum_hamiltonian_rank': 1,  # maximum_h_rank
+#     'maximum_coupled_cluster_rank': 1,  # maximum_cc_rank
+#     's_taylor_max_order': 1,
+#     'omega_max_order': 1,
+# }
+
+# test2={
+#     'maximum_hamiltonian_rank': 1,  # maximum_h_rank
+#     'maximum_coupled_cluster_rank': 1,  # maximum_cc_rank
+#     's_taylor_max_order': 1,
+#     'omega_max_order': 1,
+# }
+# print(test[1])
+# _verify_fcc_truncations(test)
