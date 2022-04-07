d46c1f4ed8e093e992bf6d3c68b02824fa1d3b7b
lucas <lhprice@uwaterloo.ca>
2022-04-06 22:36:56 -0400

    updated truncations.py -basic truncationskeys enum class -loading/saving to json funcitonal but needes some minor tweaking

--- truncations.py
+++ truncations.py
@@ -1,8 +1,10 @@
 # system imports
 import os
+import copy
+import json
 import itertools as it
 from collections import namedtuple
-
+from enum import Enum
 # third party imports
 
 # local imports
@@ -38,10 +40,6 @@ fcc_key_list = [
     's_taylor_max_order',
     'omega_max_order'
 ]
-
-
-fcc_truncations_namedtuple = namedtuple('fcc_truncations', fcc_key_list)
-
 eT_z_t_key_list = [
     'maximum_hamiltonian_rank',
     'maximum_coupled_cluster_rank',
@@ -49,11 +47,63 @@ eT_z_t_key_list = [
     'eT_taylor_max_order',
     'omega_max_order'
 ]
+fcc_truncations_namedtuple = namedtuple('fcc_truncations', fcc_key_list)
 eT_z_t_truncations_namedtuple = namedtuple('eT_z_t_truncations', eT_z_t_key_list)
 
+""" contains the Truncations Keys """
+
+class TruncationsKeys(Enum):
+    """The TruncationsKeys, which are the keys (strings) used in the .json files to identify the corresponding values
+    """
+
+    max_h_rank = 'maximum_hamiltonian_rank'
+    max_cc_rank = 'maximum_coupled_cluster_rank'
+    max_s_order = 's_taylor_max_order'
+    max_proj = 'omega_max_order'
+    max_T_rank = 'maximum_T_rank'
+    max_eT_order = 'eT_taylor_max_order'
+
+    # aliases for the enum members
+    H = max_h_rank
+    CC = max_cc_rank
+    S = max_s_order
+    P = max_proj
+    T = max_T_rank
+    eT = max_eT_order
+
+    @classmethod
+    def change_dictionary_keys_from_enum_members_to_strings(cls, input_dict):
+        """ does what it says """
+        for key, value in list(input_dict.items()):
+            if key in cls:
+                input_dict[key.value] = value
+                del input_dict[key]
+        return
+
+    @classmethod
+    def change_dictionary_keys_from_strings_to_enum_members(cls, input_dict):
+        """ does what it says """
+        for key, value in list(input_dict.items()):
+            input_dict[cls(key)] = value
+            del input_dict[key]
+        return
+
+    @classmethod
+    def fcc_key_list(cls):
+        """ returns a list of all enum members that are omitted from the .json file if all of their array's values are 0
+        """
+        #  maximum_h_rank, maximum_cc_rank, s_taylor_max_order, omega_max_order = truncations
+        return [cls.H, cls.CC, cls.S, cls.P]
+
+    @classmethod
+    def eT_key_list(cls):
+        """ returns a list of all enum members that are omitted from the .json file if all of their array's values are 0
+        """
+        #  maximum_h_rank, maximum_cc_rank, maximum_T_rank, eT_taylor_max_order, omega_max_order = truncations
+        return [cls.H, cls.CC, cls.T, cls.eT, cls.P]
+
 ###################################################################
-"""
-Verify concept
+"""Verify concept
 """
 # below assumes truncations is type==dictionary
 
@@ -105,3 +155,38 @@ def _verify_eT_z_t_truncations(truncations):
 # }
 # print(test[1])
 # _verify_fcc_truncations(test)
+
+################################################################################
+#                              Save / Load JSON                                #
+################################################################################
+
+
+def _save_to_JSON(path, dictionary):
+    dict_copy = copy.deepcopy(dictionary)
+    TruncationsKeys.change_dictionary_keys_from_enum_members_to_strings(dict_copy)
+    with open(path, mode='w', encoding='UTF8') as target_file:
+        target_file.write(json.dumps(dict_copy))
+
+    return
+
+def save_model_to_JSON(path, dictionary):
+    """ wrapper for _save_to_JSON
+    calls verify_model_parameters() before calling _save_to_JSON()
+    """
+    # verify_model_parameters(dictionary)  modify _verify_func
+    # log.debug(f"Saving model to {path:s}")
+    _save_to_JSON(path, dictionary)
+    return
+
+def _load_from_JSON(path):
+    """returns a dictionary filled with the values stored in the .json file located at path"""
+
+    with open(path, mode='r', encoding='UTF8') as file:
+        input_dictionary = json.loads(file.read())
+
+    TruncationsKeys.change_dictionary_keys_from_strings_to_enum_members(input_dictionary)
+
+    for key, value in input_dictionary.items():
+        input_dictionary[key] = value
+    # add verify
+    return input_dictionary
