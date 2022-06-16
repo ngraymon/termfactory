3823488540761df61b6954cac9a11fcddefd06eb
Neil <ngraymon@uwaterloo.ca>
2022-06-16 19:43:29 -0400

    Fix - tests

--- tests/deep/files/test_code_full_cc/generate_full_cc_python_file_contents_out.py
+++ tests/deep/files/test_code_full_cc/generate_full_cc_python_file_contents_out.py
@@ -336,5 +336,199 @@ def compute_m1_n0_amplitude_optimized(A, N, ansatz, truncation, h_args, t_args, 
     add_m1_n0_unlinked_disconnected_terms_optimized(R, ansatz, truncation, h_args, t_args, opt_unlinked_path_list)
     return R
 
+# ------------------------------------------------------------------------------------------------------------- #
+# ----------------------------------------- OPTIMIZED PATHS FUNCTIONS ----------------------------------------- #
+# ------------------------------------------------------------------------------------------------------------- #
 
-# --------------------------------------------- OPTIMIZED PATHS FUNCTION --------------------------------------------- #
+# ----------------------------------------- INDIVIDUAL OPTIMIZED PATHS ----------------------------------------- #
+
+
+# ------------ operator(name='', rank=0, m=0, n=0) OPTIMIZED PATHS ------------ #
+def compute_m0_n0_fully_connected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the fully_connected terms."""
+
+    fully_connected_opt_path_list = []
+
+    if ansatz.ground_state:
+
+        if truncation.at_least_linear:
+            if truncation.singles:
+                fully_connected_opt_path_list.append(oe.contract_expression((A, A, N), (A, A, N)))
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return fully_connected_opt_path_list
+
+
+def compute_m0_n0_linked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the linked_disconnected terms."""
+
+    linked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return linked_disconnected_opt_path_list
+
+
+def compute_m0_n0_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the unlinked_disconnected terms."""
+
+    unlinked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return unlinked_disconnected_opt_path_list
+
+# --------------------------------------------------------------------------- #
+# ------------------------- RANK  1 OPTIMIZED PATHS ------------------------- #
+# --------------------------------------------------------------------------- #
+
+
+# ------------ operator(name='b', rank=1, m=0, n=1) OPTIMIZED PATHS ------------ #
+def compute_m0_n1_fully_connected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the fully_connected terms."""
+
+    fully_connected_opt_path_list = []
+
+    if ansatz.ground_state:
+
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return fully_connected_opt_path_list
+
+
+def compute_m0_n1_linked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the linked_disconnected terms."""
+
+    linked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return linked_disconnected_opt_path_list
+
+
+def compute_m0_n1_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the unlinked_disconnected terms."""
+
+    unlinked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        if truncation.singles:
+            unlinked_disconnected_opt_path_list.append(oe.contract_expression((A, A), (A, A, N)))
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return unlinked_disconnected_opt_path_list
+
+
+# ------------ operator(name='d', rank=1, m=1, n=0) OPTIMIZED PATHS ------------ #
+def compute_m1_n0_fully_connected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the fully_connected terms."""
+
+    fully_connected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return fully_connected_opt_path_list
+
+
+def compute_m1_n0_linked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the linked_disconnected terms."""
+
+    linked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return linked_disconnected_opt_path_list
+
+
+def compute_m1_n0_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation):
+    """Calculate optimized einsum paths for the unlinked_disconnected terms."""
+
+    unlinked_disconnected_opt_path_list = []
+
+    if ansatz.ground_state:
+        pass  # no valid terms here
+    else:
+        raise Exception('Hot Band amplitudes not implemented properly and have not been theoretically verified!')
+
+    return unlinked_disconnected_opt_path_list
+
+
+# ----------------------------------------- GROUPED BY PROJECTION OPERATOR ----------------------------------------- #
+def compute_m0_n0_optimized_paths(A, N, ansatz, truncation):
+    """Compute the optimized paths for this operator(name='', rank=0, m=0, n=0)."""
+    truncation.confirm_at_least_singles()
+
+    connected_opt_path_list = compute_m0_n0_fully_connected_optimized_paths(A, N, ansatz, truncation)
+    linked_opt_path_list = compute_m0_n0_linked_disconnected_optimized_paths(A, N, ansatz, truncation)
+    unlinked_opt_path_list = compute_m0_n0_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation)
+
+    return_dict = {
+        (0, 0): [connected_opt_path_list, linked_opt_path_list, unlinked_opt_path_list]
+    }
+
+    return return_dict
+
+
+def compute_m0_n1_optimized_paths(A, N, ansatz, truncation):
+    """Compute the optimized paths for this operator(name='b', rank=1, m=0, n=1)."""
+    truncation.confirm_at_least_singles()
+
+    connected_opt_path_list = compute_m0_n1_fully_connected_optimized_paths(A, N, ansatz, truncation)
+    linked_opt_path_list = compute_m0_n1_linked_disconnected_optimized_paths(A, N, ansatz, truncation)
+    unlinked_opt_path_list = compute_m0_n1_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation)
+
+    return_dict = {
+        (0, 1): [connected_opt_path_list, linked_opt_path_list, unlinked_opt_path_list]
+    }
+
+    return return_dict
+
+
+def compute_m1_n0_optimized_paths(A, N, ansatz, truncation):
+    """Compute the optimized paths for this operator(name='d', rank=1, m=1, n=0)."""
+    truncation.confirm_at_least_singles()
+
+    connected_opt_path_list = compute_m1_n0_fully_connected_optimized_paths(A, N, ansatz, truncation)
+    linked_opt_path_list = compute_m1_n0_linked_disconnected_optimized_paths(A, N, ansatz, truncation)
+    unlinked_opt_path_list = compute_m1_n0_unlinked_disconnected_optimized_paths(A, N, ansatz, truncation)
+
+    return_dict = {
+        (1, 0): [connected_opt_path_list, linked_opt_path_list, unlinked_opt_path_list]
+    }
+
+    return return_dict
+
+
+# ----------------------------------------- MASTER OPTIMIZED PATH FUNCTION ----------------------------------------- #
+def compute_all_optimized_paths(A, N, ansatz, truncation):
+    """Return dictionary containing optimized contraction paths.
+    Calculates all optimized paths for the `opt_einsum` calls up to
+        a maximum order of m+n=1 for a projection operator P^m_n
+    """
+    all_opt_path_lists = []
+
+    all_opt_path_lists[(0, 0)] = compute_m0_n0_optimized_paths(A, N, ansatz, truncation)[(0, 0)]
+    all_opt_path_lists[(0, 1)] = compute_m0_n1_optimized_paths(A, N, ansatz, truncation)[(0, 1)]
+    all_opt_path_lists[(1, 0)] = compute_m1_n0_optimized_paths(A, N, ansatz, truncation)[(1, 0)]
+
+    return all_opt_path_lists
+
+
